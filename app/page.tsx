d8d47d013b05171ba4d17df76daaf7256f3ba81e
SergioGaitan27 <sergiogaitan203@gmail.com>
2024-08-22 23:29:35 -0600

    1.9 redirección en la raiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,29 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { status } = useSession();
   const router = useRouter();
 
-  const showSession = () => {
-    if (status === "authenticated") {
-      return (
-        <button
-          className="border border-solid border-black rounded"
-          onClick={() => {
-            signOut({ redirect: false }).then(() => {
-              router.push("/");
-            });
-
-          }}
-        >
-          Sign Out
-        </button>
-      )
-    } else if (status === "loading") {
-      return (
-        <span className="text-[#888] text-sm mt-7">Loading...</span>
-      )
-    } else {
-      return (
-        <Link
-          href="/login"
-          className="border border-solid border-black rounded"
-        >
-          Sign In
-        </Link>
-      )
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    } else if (status === "authenticated") {
+      router.push("/dashboard");
     }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <span className="text-[#888] text-sm">Loading...</span>
+      </main>
+    );
   }
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-xl">Home</h1>
-      {showSession()}
-    </main>
-  );
+
+  // Este return nunca debería ser alcanzado, pero lo mantenemos para evitar errores
+  return null;
 }
