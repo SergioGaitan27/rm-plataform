ba67a0a1f88ea0fe5e482fae68ed46850e7c3169
SergioGaitan27 <sergiogaitan203@gmail.com>
2024-08-30 16:59:24 -0600

    4.0 Tiempo real con Pusher 3ero

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,6 +2,7 @@ import Pusher from 'pusher-js';
 
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  forceTLS: true
 });
 
 export default pusher;
